570b4745cbc77caec05f3392542ddc70b191ebb7
Michael Hanson <186724+mybuddymichael@users.noreply.github.com>
2021-01-18 16:45:38 -0800

    Create an iterative version of the maze

--- maze/sketch.js
+++ maze/sketch.js
@@ -1,19 +1,29 @@
-const canvasX = 600;
-const canvasY = 600;
 const margin = 40;
 
+const step = 10;
+
 function setup() {
-  createCanvas(canvasX, canvasY, SVG);
-  noLoop();
+  createCanvas(600, 600, SVG);
+  strokeWeight(3);
+  frameRate(300);
+  background('peachpuff');
 }
 
+let currentX = 1;
+let currentY = 1;
+
 function draw() {
-  const step = 10;
-  for (let x = margin; x < canvasX - margin; x = x + step) {
-    for (let y = margin; y < canvasY - margin; y = y + step) {
-      strokeWeight(3);
-      drawRandomLine(x, y, step);
-    }
+  const thisX = currentX * step + margin;
+  const thisY = currentY * step + margin;
+  drawRandomLine(thisX, thisY, step);
+  currentX += 1;
+  print(currentX);
+  if (thisX > width - margin * 2) {
+    currentX = 1;
+    currentY += 1;
+  }
+  if (thisY > height - margin * 2) {
+    noLoop();
   }
 }
 
