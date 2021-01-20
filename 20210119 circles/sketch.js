a8a96c2b97441ff44c32802d8ce9fd19487d37e4
Michael Hanson <186724+mybuddymichael@users.noreply.github.com>
2021-01-19 22:24:10 -0800

    Make the circles sketch work

--- 20210119 circles/sketch.js
+++ 20210119 circles/sketch.js
@@ -1,9 +1,12 @@
-const containerRadius = 200;
+const containerRadius = 300;
 const maxCircleRadius = 8;
-const minCircleRadius = 2;
+const minCircleRadius = 1;
+const circleMargin = 3;
+const maxTries = 300;
+const totalCircles = 2500;
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(800, 800);
   noLoop();
 }
 
@@ -12,32 +15,42 @@ function draw() {
   const middleX = width / 2;
   const middleY = height / 2;
   const containerCircle = createCircle(middleX, middleY, containerRadius);
+  const circles = createCircles(totalCircles, containerCircle);
   stroke(0);
-  circle(middleX, middleY, containerRadius * 2);
-  // Define its radius.
-  // Draw X amount of circles, randomly
-  // If the circle is overlapping another circle, try to come up with new coordinates
+  circles.map((c) => {
+    if (c) {
+      circle(c.x, c.y, c.radius * 2);
+    }
+  });
 }
 
 function createCircle(x, y, radius) {
   return { x: x, y: y, radius: radius };
 }
 
-function createCircles(n, containerCircle, circles = []) {
-  if (n <= 0) return;
-  let circle = createSafeCircle(containerCircle, circles, 10);
-  // Each circle should be within the circle's radius
-  // If a circle is not, try to come up with new coordinates
-  createCircles(n - 1, middleX, middleY);
-}
-
-function createSafeCircle({ x, y, radius }, circles, maxTries) {
-  if (maxTries <= 0) return;
+function createSafeCircle(containerCircle, circles, maxTries) {
+  let { x, y, radius } = containerCircle;
+  if (maxTries <= 0) return null;
   let circle = createCircle(
     random(x - radius, x + radius),
     random(y - radius, y + radius),
     random(minCircleRadius, maxCircleRadius + 1)
   );
+  if (
+    anyOverlap(circle, circles) ||
+    isOutsideContainer(circle, containerCircle)
+  ) {
+    return createSafeCircle(containerCircle, circles, maxTries - 1);
+  } else {
+    return circle;
+  }
+}
+
+function createCircles(n, containerCircle, circles = []) {
+  if (n <= 0) return circles;
+  let circle = createSafeCircle(containerCircle, circles, maxTries);
+  circles = [circle, ...circles];
+  return createCircles(n - 1, containerCircle, circles);
 }
 
 function isOutsideContainer(
@@ -52,11 +65,19 @@ function isOutsideContainer(
 function overlaps({ x, y, radius }, { x: x2, y: y2, radius: radius2 }) {
   const distance = dist(x, y, x2, y2);
   const actualDistance = distance - radius - radius2;
-  if (actualDistance < 1) return true;
+  if (actualDistance < circleMargin) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function anyOverlap(circle, circles) {
   return circles.some((c) => {
-    overlaps(circle, c);
+    if (!circle || !c) {
+      return false;
+    } else {
+      return overlaps(circle, c);
+    }
   });
 }
